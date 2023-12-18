3f697408d2796a2e1095efe8ac277793b938eb47
owen-ackerman <72002696+owen-ackerman@users.noreply.github.com>
2023-12-18 08:28:16 -0500

    Add files via upload

---
+++ OwenEmid/OwenMidtermArduino/OwenMidtermArduino.ino
@@ -0,0 +1,217 @@
+#include <Arduino.h>
+//#include <OSCBoards.h>
+//#include <SLIPEncodedSerial.h>
+//
+//#ifdef BOARD_HAS_USB_SERIAL
+//SLIPEncodedUSBSerial SLIPSerial( thisBoardsSerialUSB );
+//#else
+//SLIPEncodedSerial SLIPSerial(Serial); // Change to Serial1 or Serial2 etc. for boards with multiple serial ports that don’t have Serial
+//#endif
+int num_pins = 25;
+const int NumFSRpins = 16;
+byte inputAnalogPins[NumFSRpins];
+const byte Input0Pin = A0;
+const byte Input1Pin = A1;
+const int MultiplexA = 11;
+const int MultiplexB = 12;
+const int MultiplexC = 13;
+const int StartStopPin = 7;
+const int SampleHoldPin = 6;
+const int OctaveUP  = 5;
+const int OctaveDown = 4;
+const int JoystickButton = 3;
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+    //start serial connection
+
+  Serial.begin(57600);
+  //start a new SLIP Packet
+  //SLIPSerial.beginPacket();
+  //send the data
+ // uint8_t FSRpins[NumFSRpins];
+ // FSRpins[] = [A0, A1, A2, A3, A4, A5, A6, A7];
+ 
+
+  for (byte i = 0; i < NumFSRpins; ++i) {
+      inputAnalogPins[i] = A0 + i; // Assuming A0, A1, A2, ..., A15 are sequential
+  }
+
+  //configure pin 2 as an input and enable the internal pull-up resistor
+  for (int i = 53; i >=27; i--) {
+  pinMode(i, INPUT_PULLUP);
+  }
+
+   pinMode(MultiplexA, OUTPUT); 
+   pinMode(MultiplexB, OUTPUT); 
+   pinMode(MultiplexC, OUTPUT); 
+   pinMode(StartStopPin,  INPUT);
+   pinMode(SampleHoldPin, INPUT );
+   pinMode(OctaveUP,    INPUT);
+   pinMode(OctaveDown, INPUT );
+   pinMode(JoystickButton, INPUT);
+  //pinMode(LeftOutApin,  INPUT_PULLUP);
+  //pinMode(LeftOutBpin,  INPUT_PULLUP);
+digitalWrite(MultiplexA, 0);
+digitalWrite(MultiplexB, 0);
+digitalWrite(MultiplexC, 0); //000
+
+}
+
+
+//void LeftRotaryEncoder() {
+//  LeftState = digitalRead(LeftOutApin);
+//  if (LeftState != LeftLastState) {
+//    if (digitalRead(LeftOutBpin) != LeftState) {
+//      LeftCounter ++;
+//    } else {
+//      LeftCounter --;
+//    }
+//  }
+//  LeftLastState = LeftState;
+//}
+
+void loop() {
+  
+  //OSCBundle bundle
+  uint8_t pin_read[num_pins]; 
+  for (int i = 53; i >=28; i--) {
+  pin_read[53-i] = !digitalRead(i);
+  }
+  //create a new OSCMessage and add some data to it
+ for (int i = 0; i <= 24; i++) {
+ Serial.print(pin_read[i]);
+ }
+
+  //for (int i = 0; i <= 8; i++){
+  for (byte i = 4; i < 16; ++i) {
+  Serial.print(" ");
+  Serial.print(analogRead(inputAnalogPins[i]));
+  }
+
+digitalWrite(MultiplexA, 0); //000
+Serial.print(" ");
+Serial.print(analogRead(inputAnalogPins[0]));
+digitalWrite(MultiplexC, 1); //001
+Serial.print(" ");
+Serial.print(analogRead(inputAnalogPins[0]));
+digitalWrite(MultiplexB, 1); //011
+Serial.print(" ");
+Serial.print(analogRead(inputAnalogPins[0]));
+digitalWrite(MultiplexC, 0); //010
+Serial.print(" ");
+Serial.print(analogRead(inputAnalogPins[0]));
+
+
+//digitalWrite(Multiplex1, 0);
+//Serial.print(" ");
+//Serial.print(analogRead(inputAnalogPins[0]));
+//  Serial.println();
+
+
+//
+//  
+//  
+digitalWrite(MultiplexA, 1);//110
+Serial.print(" ");
+Serial.print(analogRead(inputAnalogPins[0]));
+digitalWrite(MultiplexC, 1);//111
+Serial.print(" ");
+Serial.print(analogRead(inputAnalogPins[0]));
+digitalWrite(MultiplexB, 0);//101
+Serial.print(" ");
+Serial.print(analogRead(inputAnalogPins[0]));
+digitalWrite(MultiplexC, 0);//100
+Serial.print(" ");
+Serial.print(analogRead(inputAnalogPins[0]));
+  Serial.print(" ");
+
+Serial.print(analogRead(inputAnalogPins[2]));
+Serial.print(" ");
+Serial.print(analogRead(inputAnalogPins[3]));
+Serial.print(" ");
+
+  Serial.print(digitalRead(StartStopPin));
+  Serial.print(" ");
+  Serial.print(digitalRead(SampleHoldPin));
+
+  Serial.print(" ");
+  Serial.print(digitalRead(OctaveUP));
+  Serial.print(" ");
+  Serial.print(digitalRead(OctaveDown));
+  Serial.print(" ");
+  Serial.print(digitalRead(JoystickButton));
+  Serial.println();
+//
+//  // put your main code here, to run repeatedly:
+//LeftRotaryEncoder();
+//
+//Serial.print("Left: ");
+//
+//Serial.println(LeftCounter);
+
+//Serial.print(digitalRead(LeftOutApin));
+//Serial.print(" ");
+//Serial.print(digitalRead(LeftOutBpin)); 
+//Serial.println();
+
+
+}
+
+
+  //for (byte i = 0; i < 1; ++i) {
+  //digitalWrite(Multiplex1, i);
+  //analogRead(inputAnalogPins[13]);
+  //}
+  //digitalWrite(Multiplex1, 1);
+  //Serial.print(" ");
+  //Serial.print(analogRead(inputAnalogPins[13]));
+  //Serial.print(" ");
+  //Serial.print(analogRead(inputAnalogPins[14]));
+  //digitalWrite(Multiplex1, 0);
+  //Serial.print(" ");
+  //Serial.print(analogRead(inputAnalogPins[13]));
+  //Serial.print(" ");
+  //Serial.print(analogRead(inputAnalogPins[14]));
+
+
+ //Serial.print(" ");
+ //Serial.print(analogRead(A1));
+ //Serial.print(" ");
+ //Serial.print(analogRead(A2));
+ //Serial.print(" ");
+ //Serial.print(analogRead(A3));
+ //Serial.print(" ");
+ //Serial.print(analogRead(A4));
+ //Serial.print(" ");
+ //Serial.print(analogRead(A5));
+ //Serial.print(" ");
+ //Serial.print(analogRead(A6));
+ //Serial.print(" ");
+ //Serial.print(analogRead(A7));
+  
+  //Serial.print(" ");
+  //Serial.print(44);
+  //Serial.print(" ");
+  //Serial.println(399);
+
+  //delay(50);
+
+  //bundle.add("/Piano/0").add(&pin_read,num_pins);
+
+//  bundle.getOSCMessage(0);//returns the OSCMessage with the address "/a".
+  //bundle.send(SLIPSerial);
+  ////read the pushbutton value into a variable
+  //
+  //bundle.empty(); 
+
+  ////end the packet
+  //SLIPSerial.endPacket();
+
+
+
+
---
+++ OwenEmid/VelocityOff.maxpat
@@ -0,0 +1,211 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 8,
+			"minor" : 3,
+			"revision" : 1,
+			"architecture" : "x64",
+			"modernui" : 1
+		}
+,
+		"classnamespace" : "box",
+		"rect" : [ 84.0, 132.0, 640.0, 480.0 ],
+		"bglocked" : 0,
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 1,
+		"gridsize" : [ 15.0, 15.0 ],
+		"gridsnaponopen" : 1,
+		"objectsnaponopen" : 1,
+		"statusbarvisible" : 2,
+		"toolbarvisible" : 1,
+		"lefttoolbarpinned" : 0,
+		"toptoolbarpinned" : 0,
+		"righttoolbarpinned" : 0,
+		"bottomtoolbarpinned" : 0,
+		"toolbars_unpinned_last_save" : 0,
+		"tallnewobj" : 0,
+		"boxanimatetime" : 200,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"description" : "",
+		"digest" : "",
+		"tags" : "",
+		"style" : "",
+		"subpatcher_template" : "",
+		"assistshowspatchername" : 0,
+		"boxes" : [ 			{
+				"box" : 				{
+					"comment" : "",
+					"id" : "obj-11",
+					"index" : 0,
+					"maxclass" : "outlet",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 346.0, 427.0, 30.0, 30.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-10",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 343.0, 336.0, 29.5, 22.0 ],
+					"text" : "0"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-8",
+					"maxclass" : "newobj",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "int" ],
+					"patching_rect" : [ 314.0, 364.0, 29.5, 22.0 ],
+					"text" : "i"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-4",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "bang", "int" ],
+					"patching_rect" : [ 285.0, 312.0, 29.5, 22.0 ],
+					"text" : "t b i"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"comment" : "",
+					"id" : "obj-6",
+					"index" : 0,
+					"maxclass" : "outlet",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 214.0, 356.0, 30.0, 30.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"comment" : "",
+					"id" : "obj-3",
+					"index" : 0,
+					"maxclass" : "inlet",
+					"numinlets" : 0,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 247.0, 110.0, 30.0, 30.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-23",
+					"maxclass" : "newobj",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 199.0, 264.0, 182.0, 22.0 ],
+					"text" : "if ($i1 == 0) && ($i2 > 0) then $i2"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-22",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 202.0, 208.0, 54.0, 22.0 ],
+					"text" : "bucket 2"
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-8", 1 ],
+					"source" : [ "obj-10", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-23", 1 ],
+					"source" : [ "obj-22", 1 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-23", 0 ],
+					"source" : [ "obj-22", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-4", 0 ],
+					"order" : 0,
+					"source" : [ "obj-23", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-6", 0 ],
+					"order" : 1,
+					"source" : [ "obj-23", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-22", 0 ],
+					"source" : [ "obj-3", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-10", 0 ],
+					"order" : 0,
+					"source" : [ "obj-4", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-8", 0 ],
+					"order" : 1,
+					"source" : [ "obj-4", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-11", 0 ],
+					"source" : [ "obj-8", 0 ]
+				}
+
+			}
+ ],
+		"dependency_cache" : [  ],
+		"autosave" : 0
+	}
+
+}
